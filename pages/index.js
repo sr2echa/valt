96c78e0dde6da0438bdc09ffbf585c06367aadf6
jmt-genius <tarunjm23@gmail.com>
2024-03-30 20:39:36 +0530

    updated getstarted

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 "use client";
 import React from 'react';
 import Link from 'next/link';
-// import './getstarted.css'; // Ensure you create this CSS file
-
+// import './getstarted.css'; // Ensure you create this CSS files
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog"
+import DropzoneComponent from './components/dropzonecomponent';
 const getstarted = () => {
   return (
     <div className="container">
@@ -13,7 +24,25 @@ const getstarted = () => {
       <h2 className="title-bold">A decentralized vault</h2>
       <p className="title-medium">to securely store your</p>
       <p className="title-medium">digital belongings</p>
-      <Link href="/files" className="get-started-button">Get Started</Link>
+      <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <button className="get-started-button">Get Started</button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Upload your Seed image here!</AlertDialogTitle>
+          <AlertDialogDescription>
+            This Image will be used as your password
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <DropzoneComponent/>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+      
     </div>
   );
 };
